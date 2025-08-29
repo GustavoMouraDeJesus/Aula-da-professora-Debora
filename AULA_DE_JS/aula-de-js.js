747956be2e0dc6c9cec51ba5200e620e4d3a8887
Gustavo Moura <gustavomouradejesus@gmail.com>
2025-08-29 17:26:07 -0300

    Aula dia 29 de Agosto sobre js

---
+++ AULA_DE_JS/aula-de-js.js
@@ -0,0 +1,23 @@
+function exibe_typer(){
+    document.getElementById("typer").style.visibility="visible";
+    document.getElementById("ram2500").style.visibility="hidden";
+    document.getElementById("cadilacescalade").style.visibility="hidden";
+}
+
+function exibe_ram2500(){
+    document.getElementById("typer").style.visibility="hidden";
+    document.getElementById("ram2500").style.visibility="visible";
+    document.getElementById("cadilacescalade").style.visibility="hidden";
+}
+
+function exibe_cadilacescalade(){
+    document.getElementById("typer").style.visibility="hidden";
+    document.getElementById("ram2500").style.visibility="hidden";
+    document.getElementById("cadilacescalade").style.visibility="visible";
+}
+
+function some_tudo(){
+    document.getElementById("typer").style.visibility="hidden";
+    document.getElementById("ram2500").style.visibility="hidden";
+    document.getElementById("cadilacescalade").style.visibility="hidden";
+}
